83cf90da45cc71a74a111158aa7ef1d9f8cd56bf
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2020-11-17 16:25:50 +0100

    :bulb: Add comment in index.js to explain mobile nav menu behaviour

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const menuOpenButton = document.querySelector(".navigation__menu");
 const menuCloseButton = document.querySelector(".navigation__menu-close");
 
 
-
+//open menu button on mobile
 navButton.addEventListener("click", function (e) {
     menuOpenButton.style.transform = "translateX(0)";
 });
 
+//close menu button on mobile
 menuCloseButton.addEventListener("click", function (e) {
    menuOpenButton.style.transform = "translateX(100%)";
 });

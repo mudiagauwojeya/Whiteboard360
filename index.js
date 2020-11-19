b071b3a760e667f200b047e92eb2f3b1500b2b45
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2020-11-19 11:39:45 +0100

    :lipstick: Add style for the media section

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const navButton = document.querySelector(".navigation__bg");
 const menuOpenButton = document.querySelector(".navigation__menu");
 const menuCloseButton = document.querySelector(".navigation__menu-close");
+const mediaControls = document.querySelectorAll(".media__file");
 
 
 //open menu button on mobile
@@ -12,3 +13,6 @@ navButton.addEventListener("click", function (e) {
 menuCloseButton.addEventListener("click", function (e) {
    menuOpenButton.style.transform = "translateX(100%)";
 });
+
+//add and remove video controls
+

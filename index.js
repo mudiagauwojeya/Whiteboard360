cb91fd3077362d303df8283fdff7a522e9ef5a8d
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-01-19 12:51:39 +0100

    :construction: Add function to close nav menu when contact link is clicked

--- index.js
+++ index.js
@@ -2,26 +2,29 @@ const navButton = document.querySelector(".navigation__bg");
 const menuOpenButton = document.querySelector(".navigation__menu");
 const menuCloseButton = document.querySelector(".navigation__menu-close");
 const mediaControls = document.querySelectorAll(".media__file");
-
+const contactLink = document.querySelector("a[href='#contact']");
 
 //open menu button on mobile
-navButton.addEventListener("click", function (e) {
-    menuOpenButton.style.transform = "translateX(0)";
-});
+const openMenuBtn = () => {
+	menuOpenButton.style.transform = "translateX(0)";
+};
+navButton.addEventListener("click", openMenuBtn);
 
 //close menu button on mobile
-menuCloseButton.addEventListener("click", function (e) {
-   menuOpenButton.style.transform = "translateX(100%)";
-});
+const closeMenuBtn = () => {
+	menuOpenButton.style.transform = "translateX(100%)";
+};
+menuCloseButton.addEventListener("click", closeMenuBtn);
 
 //add and remove video controls
 function toggleControls() {
-    for (i = 0; i < mediaControls.length; i++) {
-        if (mediaControls[i].hasAttribute("controls")) {
-            mediaControls[i].removeAttribute("controls");
-        } else {
-            mediaControls[i].setAttribute("controls", "controls");
-        }
-    }
+	for (i = 0; i < mediaControls.length; i++) {
+		if (mediaControls[i].hasAttribute("controls")) {
+			mediaControls[i].removeAttribute("controls");
+		} else {
+			mediaControls[i].setAttribute("controls", "controls");
+		}
+	}
 }
 
+contactLink.addEventListener("click", closeMenuBtn);

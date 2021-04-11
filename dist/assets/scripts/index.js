6c557c3012de78fe6298738eec81926609cbd2b3
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-04-11 19:46:41 +0100

    :recycle: Refactor the observer

--- dist/assets/scripts/index.js
+++ dist/assets/scripts/index.js
@@ -139,8 +139,8 @@ if (modalForm) {
 document.addEventListener("DOMContentLoaded", () => {
 	const options = {
 		root: null,
-		rootMargin: "0px 0px -5px",
-		threshold: 0.03,
+		rootMargin: "0px",
+		threshold: 0.05,
 	};
 	const slideIn = (elements, observer) => {
 		for (const element of elements) {
